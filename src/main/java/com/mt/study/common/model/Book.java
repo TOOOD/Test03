2a05b8d24d690fa19d2168fe1ca72bc0be32989f
TOOOD <1131014001@qq.com>
2021-03-06 18:03:26 +0800

    002

---
+++ src/main/java/com/mt/study/common/model/Book.java
@@ -0,0 +1,4 @@
+package com.mt.study.common.model;
+
+public class Book {
+}
--- src/main/java/com/mt/study/web/DemoController.java
+++ src/main/java/com/mt/study/web/DemoController.java
@@ -1,9 +1,10 @@
 package com.mt.study.web;
 
 import com.mt.study.service.DemoServiceImpl;
+import org.junit.Test;
 
 public class DemoController {
-
+@Test
     public void demo01() {
         DemoServiceImpl demo = new DemoServiceImpl();
         try {
@@ -13,12 +14,12 @@ public class DemoController {
         }
     }
 
-    public static void main(String[] args) {
-        DemoServiceImpl demo = new DemoServiceImpl();
-        try {
-            System.out.println(demo.randomNumber(5));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        DemoServiceImpl demo = new DemoServiceImpl();
+//        try {
+//            System.out.println(demo.randomNumber(5));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
